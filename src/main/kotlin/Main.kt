2025ea887bf62e8e8cdc022f129fdd11e082d119
zhitm <zhitnuhina.maria@gmail.com>
2021-10-22 19:09:47 +0300

    fixed path

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,6 @@
 fun main(args: Array<String>) {
     val solver: Solver = Solver()
-    solver.readFormulaFromFile("/home/maria/IdeaProjects/SAT-solver/src/main/kotlin/test.txt")
+    solver.readFormulaFromFile("src/main/kotlin/test.txt")
     solver.solve()
 
 }
